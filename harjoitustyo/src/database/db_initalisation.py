556e7efb10ea57ee5675446cf8dc94584e93ba53
inanimateobject <mirko.vonbell@helsinki.fi>
2022-04-08 00:52:16 +0300

    syntax error fixed

--- harjoitustyo/src/database/db_initalisation.py
+++ harjoitustyo/src/database/db_initalisation.py
@@ -2,13 +2,16 @@
 Init script, sets up database
 """
 import sqlite3
+import os
 
 
 def initialize():
-    with open("database_structure.sql", "r") as dbs:
+    dirname = os.path.dirname(__file__)
+
+    with open(os.path.join(dirname, 'database_structure.sql'), "r") as dbs:
         database_script = dbs.read()
 
-    db = sqlite3.connect("velkakanta.db")
+    db = sqlite3.connect(os.path.join(dirname, 'velkakanta.db'))
     with db:
         cur = db.cursor()
         cur.executescript(database_script)
